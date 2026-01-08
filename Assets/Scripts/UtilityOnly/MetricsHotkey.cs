66183647184315419942196ae9bc7d0d46629c85
Condotierro <davidosx09@gmail.com>
2026-01-08 18:40:35 +0100

    basic recording

---
+++ Assets/Scripts/UtilityOnly/MetricsHotkey.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetricsHotkey : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            RuntimeMetrics.SaveToFile();
+        }
+    }
+}
